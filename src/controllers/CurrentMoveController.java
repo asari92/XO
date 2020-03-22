f4b84679b802706613cef426c0190638744eedeb
asari92 <dulmaev.andrei@gmail.com>
2020-03-22 20:53:47 +0800

    CurrentMoveController - created

---
+++ src/controllers/CurrentMoveController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+            countFigure += countFiguresIntTheRow(field, x);
+        }
+
+        if (countFigure == field.getSize() * field.getSize()) {
+            return null;
+        }
+
+        if (countFigure % 2 == 0) {
+            return Figure.X;
+        }
+
+        return Figure.O;
+
+    }
+
+    private int countFiguresIntTheRow(final Field field, final int row) {
+        int countFigure = 0;
+        for (int y = 0; y < field.getSize(); y++) {
+            try {
+                if (field.getFigure(new Point(row, y)) != null) {
+                    countFigure++;
+                }
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+        return countFigure;
+    }
+
+}
