b3591e4cd6c984d723d709f737867b9f655c2138
asari92 <dulmaev.andrei@gmail.com>
2020-03-23 01:14:00 +0800

    CLI v1

---
+++ src/XOCLI.java
@@ -0,0 +1,26 @@
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.Player;
+import view.ConsoleView;
+
+public class XOCLI {
+
+    public static void main(final String[] args) {
+        final String name1 = "Gleb";
+        final String name2 = "Slava";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
+
+        final Game gameXO = new Game(new Field(3), "XO", players);
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+        while (consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
+        }
+    }
+
+}
